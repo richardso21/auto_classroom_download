e22842ef61243c2ebca7f93d3949466f76b07c4d
richardso21 <richiso899@yahoo.com>
2019-12-10 13:19:00 -0500

    enable save w/ original file name

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import argparse
+import pandas as pd
 import classroom
 import drive
 from classroom import parse_courses, get_id
@@ -5,11 +7,7 @@ from drive import download_from_drive
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description='only if you know what you\'re doing')
-    # parser.add_argument('')
 
     ids_df, selection = parse_courses()
-
     materials_df, materials_selection = get_id(ids_df, selection)
-
     download_from_drive(materials_df, materials_selection)
--- drive.py
+++ drive.py
@@ -4,8 +4,9 @@ from pydrive.drive import GoogleDrive
 def download_from_drive(materials_df, materials_selection):
 
     id_file = materials_df.iloc[materials_selection]['id']
+    file_name = materials_df.iloc[materials_selection]['title']
 
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
     file_down = drive.CreateFile({'id': id_file})
-    file_down.GetContentFile('output.pdf')
+    file_down.GetContentFile('{}'.format(file_name))
