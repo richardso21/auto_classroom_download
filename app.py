f4dd7ca734f412d8b5c77d2d884d14e9364cd25a
richardso21 <richiso899@yahoo.com>
2019-12-09 20:48:34 -0500

    update interfaces

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import pickle, sys
-import os.path
+import pickle, sys, os, argparse
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -10,53 +9,87 @@ import pandas as pd
 SCOPES = ['https://www.googleapis.com/auth/classroom.courses',
           'https://www.googleapis.com/auth/classroom.announcements']
 
+
 def parse_courses():
     results = service.courses().list(courseStates='ACTIVE').execute()
     courses = results['courses']
     ids = []
-    for n,i in enumerate(courses):
-        ids.append({'name':courses[n]['name'],'id':courses[n]['id']})
+    for n, i in enumerate(courses):
+        ids.append({'name': courses[n]['name'], 'id': courses[n]['id']})
     ids_df = pd.DataFrame(ids)
 
-    print('Here is a list of your google classrooms:','\n')
-    print('#',' ','Classroom Name')
+    print('Here is a list of your google classrooms:', '\n')
+    print('#', ' ', 'Classroom Name')
     print('----------')
-    for n,i in enumerate(ids_df['name']):
-        print(n,' ',i)
+    for n, i in enumerate(ids_df['name']):
+        print(n, ' ', i)
 
-    selection = int(input('Which classroom do you want to get files from? Enter the corresponding number with the name of the classroom: '))
+    selection = input(
+        'Which classroom do you want to get files from? Enter the corresponding number with the name of the classroom or quit with \'q\': ')
+
+    while selection == '':
+        selection = input('Please select a classroom')
+
+    if selection == 'q' or selection == 'Q':
+        sys.exit('Quitting...')
+
+    selection = int(selection)
+
     return ids_df, selection
-
 
 
 def get_id(ids_df, selection):
     """Basic usage of the Classroom API."""
     courseid = ids_df.iloc[selection]['id']
 
-    results = service.courses().announcements().list(courseId=courseid).execute()
+    results = service.courses().announcements().list(
+        courseId=courseid).execute()
     announcements = results.get('announcements', [])
 
-    for i in range(2): #announcements[0] and [1]
-        if "materials" in announcements[i].keys():
-            # print(announcements[i]["materials"])
-            drive_direct = announcements[i]['materials']
-            file_id = drive_direct[0]['driveFile']['driveFile']['id']
-            print('Aha! The id is: ',file_id)
-        else:
-            print('no pdf yet... searching on next recent post')
-            file_id = 0
-    
-    return file_id
+    materials = [announcements[n]['materials'][0] for n, i in enumerate(
+        announcements) if 'materials' in announcements[n].keys()]
+    materials_drive = [materials[n]['driveFile']['driveFile']
+                       for n, i in enumerate(materials) if 'driveFile' in materials[n].keys()]
+    materials_drive_df = pd.DataFrame(materials_drive).head(10)
+
+    try:
+        materials_drive_df['title']
+    except:
+        sys.exit('No files to parse')
+
+    print('Listing most recent posts...')
+    print('#', ' ', 'Post name')
+    print('----------')
+
+    for n, i in enumerate(materials_drive_df['title']):
+        print(n, ' ', i)
+
+    selection = input(
+        'Which pdf do you want to download? (Select by entering # or quit with \'q\'): ')
+    if selection == '':
+        selection = 0
+    elif selection == 'q' or selection == 'Q':
+        sys.exit('Quitting...')
+    else:
+        selection = int(selection)
+
+    return materials_drive_df, selection
 
 
-def download_from_drive(id_file):
+def download_from_drive(materials_df, materials_selection):
+
+    id_file = materials_df.iloc[materials_selection]['id']
+
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
-    file_down = drive.CreateFile({'id':id_file})
+    file_down = drive.CreateFile({'id': id_file})
     file_down.GetContentFile('output.pdf')
 
 
 if __name__ == '__main__':
+    # parser = argparse.ArgumentParser(description='only if you know what you\'re doing')
+    # parser.add_argument('')
+
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
@@ -72,12 +105,10 @@ if __name__ == '__main__':
             pickle.dump(creds, token)
 
     service = build('classroom', 'v1', credentials=creds)
-    
-    ###---------------------------------
+
+    # ---------------------------------
     ids_df, selection = parse_courses()
 
-    fileID = get_id(ids_df, selection)
-    if fileID == 0:
-        sys.exit('There are currently no recent files to parse')
-    
-    download_from_drive(fileID)
+    materials_df, materials_selection = get_id(ids_df, selection)
+
+    download_from_drive(materials_df, materials_selection)
