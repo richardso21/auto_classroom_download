0a76a7a177664dbfcd4878fba10139c5ee8c91c5
richardso21 <richiso899@yahoo.com>
2019-12-10 13:38:36 -0500

    output folder integration

--- drive.py
+++ drive.py
@@ -1,5 +1,6 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import os
 
 def download_from_drive(materials_df, materials_selection):
 
@@ -9,4 +10,6 @@ def download_from_drive(materials_df, materials_selection):
     gauth = GoogleAuth()
     drive = GoogleDrive(gauth)
     file_down = drive.CreateFile({'id': id_file})
-    file_down.GetContentFile('{}'.format(file_name))
+    if os.path.exists('output') == False:
+        os.mkdir('output')
+    file_down.GetContentFile('output/{}'.format(file_name))
